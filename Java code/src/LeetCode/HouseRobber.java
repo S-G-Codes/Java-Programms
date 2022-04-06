d2e9836e2d77fe8bf84716a80262a1a19475229f
SGCodes <sanketghule18@gmail.com>
2022-04-06 23:16:21 +0530

    Not a Good Day for Question - Algo day 12

---
+++ Java code/src/LeetCode/HouseRobber.java
@@ -0,0 +1,37 @@
+public class HouseRobber {
+    // https://leetcode.com/problems/house-robber/
+
+    //using dp
+    //Tc = O(n)
+    //space O(n)  However it can be reduced by using variables instead of dp
+    public int rob(int[] nums) {
+         //Some edge cases
+
+         if(nums.length ==0) return 0;
+
+         if(nums.length ==1) return nums[0];
+
+         if(nums.length ==2) return Math.max(nums[0], nums[1]);
+
+         
+         int [] dp = new int[nums.length];
+         dp[0] = nums[0];
+         dp[1]= nums[1];
+         dp[2]= nums[0]+ nums[2];
+
+         int max = Math.max(dp[2], dp[1]);
+
+         for (int i = 3; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
+             max = Math.max(dp[i] , max);
+         }
+         return max;
+    }
+
+    public static void main(String[] args) {
+    
+        
+
+
+    }
+}
