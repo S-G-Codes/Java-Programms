43dc6a1e4d5999e4ad11dd5401e328b8c33b1698
SGCodes <sanketghule18@gmail.com>
2022-05-02 23:33:16 +0530

    Leetcode

---
+++ Java code/src/LeetCode/shortArraybyParity.java
@@ -0,0 +1,18 @@
+public class shortArraybyParity {
+    // https://leetcode.com/problems/sort-array-by-parity/submissions/
+    public int[] sortArrayByParity(int[] nums) {
+        int count = 0;
+        for(int i = 0 ; i< nums.length; i++){
+            if(nums[i] % 2 ==0){
+                int temp = nums[count];
+                nums[count] = nums[i];
+                nums[i] = temp;
+                count++;
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
