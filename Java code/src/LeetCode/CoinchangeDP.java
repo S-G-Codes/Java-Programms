fc3e0b2b527060767918540666c7d4842367b1e5
SGCodes <sanketghule18@gmail.com>
2022-05-21 21:33:24 +0530

    Leetcode Daily

--- Java code/src/LeetCode/CoinchangeDP.java
+++ Java code/src/LeetCode/CoinchangeDP.java
@@ -1,39 +1,36 @@
+import java.util.Arrays;
+
 public class CoinchangeDP {
 
+    // approch 
+    //1. using dp we will find how to reach a certain amount with possible coin given 
+    //2. taking math.min
 
     public int coinChange(int[] coins, int amount) {
-        
-        //initalize a dp array of size amount +1
-        int [] dp = new int[amount+1];   
-        //put Integer.Max_Value Intially in it
-        Arrays.fill(dp , Integer.MAX_VALUE);
-        
-        //and Zeroth element will be always zero
-        dp[0] =0;
-        
-        
-        //Run a loop for each join
-        for(int i =0; i < coins.length; i++){
+        int dp[]= new int[amount+1];
 
-            //Run a loop in dp array and check if amount to made is less than the coins given then it not possible
-            for (int j = coins[i]; j < dp.length; j++) {
-                
-                if(dp[j - coins[i]] != Integer.MAX_VALUE){
-                       
-                    //this will be min value for us +1 because we are using coins[i] coins. Uskoh toh beh lena padegha Imagine our sol in Rec Tree format you will get it
-                    int ans = dp[j - coins[i]] +1;
-                    
+        // filling all index with infinity first
+        Arrays.fill(dp, Integer.MAX_VALUE);
+       
+        //filling 0 index with 0 bcz to reach amount 0 we need 0 coins
+        dp[0]= 0;
 
-                    //store the min one in dpth j element
-                    dp[j] = Math.min(dp[j], ans);
-
-
-                }
-            }
+        for (int i = 1; i <= amount; i++) {
+             for (int coin : coins) {
+                 if(i -  coin >=0 && dp[i-coin] != Integer.MAX_VALUE){
+                     dp[i] = Math.min(dp[i], dp[i-coin] +1);
+                 }
+             }
         }
 
-                //and if the amount is less than then coint then the above two loop will not run so we will return -1 as below else our ans 
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        //this means theres no way to achieve that amount/target
+        if(dp[amount] == Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return dp[amount];
+        }
+
+        
     }
 
     public static void main(String[] args) {
