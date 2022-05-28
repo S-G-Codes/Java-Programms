063b6026c70bd7ba31fb7ec6795b18469583849d
SGCodes <sanketghule18@gmail.com>
2022-05-28 07:51:13 +0530

    Leetcode Daily of Yesterday

---
+++ Java code/src/LeetCode/No_of_Steps_to_reduce_to_zero.java
@@ -0,0 +1,24 @@
+public class No_of_Steps_to_reduce_to_zero {
+    // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ //   public int numberOfSteps(int num) {
+        //brute force
+//         int count =0;
+        
+//         while(num != 0){
+//             count++;
+//             if(num%2 ==0){
+//                 num = num/2;
+//             }else{
+//               num =   num-1;
+//             }
+//         }
+    
+//         return count;
+//    }
+
+
+ //bit manipulation
+public int numberOfSteps(int num) {
+
+}
+}
