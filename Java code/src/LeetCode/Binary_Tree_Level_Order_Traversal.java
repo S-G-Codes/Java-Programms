f1348154f8d82a3739a33be3d20bf54d920a7c64
SGCodes <sanketghule18@gmail.com>
2022-07-14 00:01:47 +0530

    Leetcode Daily

--- Java code/src/LeetCode/Binary_Tree_Level_Order_Traversal.java
+++ Java code/src/LeetCode/Binary_Tree_Level_Order_Traversal.java
@@ -1,10 +1,63 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-import javax.swing.tree.TreeNode;
+// import javax.swing.tree.TreeNode;
+
+
+
+class TreeNode{
+          int val;
+          TreeNode left;
+          TreeNode right;
+          
+        TreeNode(int val,  TreeNode left ,  TreeNode right ){
+         this.val = val;
+         this.left = left;
+         this.right = right;
+        }
+}
+
 
 public class Binary_Tree_Level_Order_Traversal {
     // https://leetcode.com/problems/binary-tree-level-order-traversal/
     public List<List<Integer>> levelOrder(TreeNode root) {
-      
+        List<List<Integer>> ansList = new LinkedList<>();
+        
+        //here we have declare our queue as TreeNode so we can check for left and right
+        Queue<TreeNode> q= new LinkedList<TreeNode>();
+
+        if(root == null) return ansList;
+
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+           
+            List<Integer> sublist = new LinkedList<Integer>();
+
+            for (int i = 0; i < size; i++){
+                //checking left side
+                 if(q.peek().left !=null){
+                     q.offer(q.peek().left);
+                 }
+                  //checking right side
+                 if(q.peek().right !=null){
+                    q.offer(q.peek().right);
+                 }
+                 
+
+                 sublist.add(q.poll().val);
+
+
+            }
+
+            ansList.add(sublist);
+
+        }
+
+
+return ansList;
     }
 }
