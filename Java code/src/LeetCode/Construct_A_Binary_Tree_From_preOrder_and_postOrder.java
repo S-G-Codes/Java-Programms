ce69148489bf165d92605bff394e694b014f82d1
SGCodes <sanketghule18@gmail.com>
2022-07-14 23:39:53 +0530

    Leetcode Daily

---
+++ Java code/src/LeetCode/Construct_A_Binary_Tree_From_preOrder_and_postOrder.java
@@ -0,0 +1,64 @@
+public class Construct_A_Binary_Tree_From_preOrder_and_postOrder {
+    // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    
+   class TreeNode{
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    TreeNode(int val , TreeNode left , TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+   }
+
+   private TreeNode root = null;
+    private int indexInPreorder = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length==0|| inorder.length==0){
+            return null;
+        }
+
+        root = buildTreeHelper(preorder, inorder, 0 ,inorder.length-1);
+        return root;
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int startInorder, int endInorder){
+        if(indexInPreorder>=preorder.length){
+            return null;
+        }
+        TreeNode newNode = new TreeNode(preorder[indexInPreorder]);
+
+        int indexInOrder = indexInInorder(inorder, startInorder, endInorder, preorder[indexInPreorder]);
+
+        if(indexInOrder==-1){
+            // Null
+            return null;
+        }
+
+        indexInPreorder++;
+
+        newNode.left = buildTreeHelper(preorder, inorder, startInorder, indexInOrder-1);
+
+        newNode.right = buildTreeHelper(preorder, inorder, indexInOrder+1, endInorder);
+
+
+        return newNode;
+    }
+
+    private int indexInInorder(int[] inorder, int start, int end, int no){
+        while(start<=end){
+            if(no==inorder[start]){
+                return start;
+            }
+            start++;
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
