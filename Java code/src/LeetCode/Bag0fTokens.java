c663434437bf22bf4869837421be9b65a65cc49c
SGCodes <sanketghule18@gmail.com>
2022-09-12 22:18:14 +0530

    Leetcode Daily

---
+++ Java code/src/LeetCode/Bag0fTokens.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Bag0fTokens {
+    // https://leetcode.com/problems/bag-of-tokens/
+
+    //two pointer approch
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length-1;
+            int score = 0; 
+            int maxScore = 0; 
+          
+            while(left<=right){
+                if(power >= tokens[left]){
+                    power -= tokens[left];
+                    left++;
+                    score++;
+                    maxScore = Math.max(score, maxScore);
+                }else if(score > power){
+                     power += tokens[right];
+                     right--;
+                     score--;
+                }else{
+                    break;
+                }
+            }
+
+            return maxScore;
+        }
+    public static void main(String[] args) {
+        
+    }
+}
